ec2c15815a95be90667dcd3f521727d4d75f1725
dshot92 <dshot92@gmail.com>
2024-09-26 13:05:19 +0200

    batman

---
+++ __init__.py
@@ -0,0 +1,37 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# ----------------------------------------------------------
+# Author: Daniele Stochino (dshot92)
+# ----------------------------------------------------------
+
+
+import importlib
+
+# from . import operators, panels, utils
+from . import panels
+#
+modules = (
+    # operators,
+    panels,
+    # utils,
+)
+#
+if "bpy" in locals():
+    # importlib.reload(operators)
+    importlib.reload(panels)
+    # importlib.reload(utils)
+#
+#
+
+
+def register():
+    for module in modules:
+        importlib.reload(module)
+        module.register()
+#
+#
+
+
+def unregister():
+    for module in reversed(modules):
+        module.unregister()
