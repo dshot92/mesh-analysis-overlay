f39b87761ef22505561bd51bf70e72e34a22cad4
dshot92 <dshot92@gmail.com>
2024-10-26 00:00:09 +0200

    feat: only when needed for overlay

--- mesh_topology_analyzer.py
+++ mesh_topology_analyzer.py
@@ -5,6 +5,7 @@
 # ----------------------------------------------------------
 
 import bmesh
+import bpy
 
 
 class MeshTopologyAnalyzer:
@@ -40,73 +41,84 @@ class MeshTopologyAnalyzer:
         # Clear previous data
         self.clear_data()
 
-        # Analyze vertices for poles and singles
-        for v in bm.verts:
-            world_pos = matrix_world @ v.co
-            edge_count = len(v.link_edges)
+        props = bpy.context.scene.GPU_Topology_Overlay_Properties
 
-            if edge_count == 0:
-                self.singles_data.append(world_pos)
-            elif edge_count > 4:
-                self.poles_data.append(world_pos)
+        # Analyze vertices for poles and singles and non-manifold verts
+        verts = props.show_poles or props.show_singles or props.show_non_manifold_verts
+        if verts:
+            for v in bm.verts:
+                world_pos = matrix_world @ v.co
+                edge_count = len(v.link_edges)
 
-            if not v.is_manifold:
-                self.non_manifold_verts_data.append(world_pos)
+                if (edge_count == 0) and props.show_singles:
+                    self.singles_data.append(world_pos)
+                elif (edge_count > 4) and props.show_poles:
+                    self.poles_data.append(world_pos)
+
+                if (not v.is_manifold) and props.show_non_manifold_verts:
+                    self.non_manifold_verts_data.append(world_pos)
 
         # Analyze non-manifold edges
-        for edge in bm.edges:
-            if not edge.is_manifold:
-                v1 = matrix_world @ edge.verts[0].co
-                v2 = matrix_world @ edge.verts[1].co
-                self.non_manifold_edges_data.extend([v1, v2])
+        edges = props.show_non_manifold_edges
+        if edges:
+            for edge in bm.edges:
+                if (not edge.is_manifold) and props.show_non_manifold_edges:
+                    v1 = matrix_world @ edge.verts[0].co
+                    v2 = matrix_world @ edge.verts[1].co
+                    self.non_manifold_edges_data.extend([v1, v2])
 
-        for face in bm.faces:
-            if len(face.verts) == 3:  # Triangle
-                normal = matrix_world.to_3x3() @ face.normal
-                verts = [matrix_world @ v.co + normal * offset for v in face.verts]
-                self.tris_data.extend(verts)
-                self.tris_normals.extend([normal] * 3)
-            elif len(face.verts) == 4:  # Quad
-                # quads_to_process.append(face)
-                verts = face.verts[:]
-                vert_normals = [matrix_world.to_3x3() @ v.normal for v in verts]
-                vert_coords = [matrix_world @ v.co for v in verts]
+        # Analyze faces
+        faces = props.show_tris or props.show_quads or props.show_ngons
+        if faces:
+            for face in bm.faces:
+                if (len(face.verts) == 3) and props.show_tris:  # Triangle
+                    normal = matrix_world.to_3x3() @ face.normal
+                    verts = [matrix_world @ v.co + normal * offset for v in face.verts]
+                    self.tris_data.extend(verts)
+                    self.tris_normals.extend([normal] * 3)
+                elif (len(face.verts) == 4) and props.show_quads:  # Quad
+                    # quads_to_process.append(face)
+                    verts = face.verts[:]
+                    vert_normals = [matrix_world.to_3x3() @ v.normal for v in verts]
+                    vert_coords = [matrix_world @ v.co for v in verts]
 
-                # Create two triangles from the quad (0,1,2) and (0,2,3)
-                tri1_indices = [0, 1, 2]
-                tri2_indices = [0, 2, 3]
+                    # Create two triangles from the quad (0,1,2) and (0,2,3)
+                    tri1_indices = [0, 1, 2]
+                    tri2_indices = [0, 2, 3]
 
-                # First triangle
-                tri1_verts = [
-                    vert_coords[i] + vert_normals[i] * offset for i in tri1_indices
-                ]
-                self.quads_data.extend(tri1_verts)
-                self.quads_normals.extend([matrix_world.to_3x3() @ face.normal] * 3)
-
-                # Second triangle
-                tri2_verts = [
-                    vert_coords[i] + vert_normals[i] * offset for i in tri2_indices
-                ]
-                self.quads_data.extend(tri2_verts)
-                self.quads_normals.extend([matrix_world.to_3x3() @ face.normal] * 3)
-            elif len(face.verts) > 4:  # N-gon
-                # ngons_to_process.append(face)
-                verts = face.verts[:]
-                vert_normals = [matrix_world.to_3x3() @ v.normal for v in verts]
-                vert_coords = [matrix_world @ v.co for v in verts]
-
-                # Create triangles by fanning from the first vertex
-                for i in range(1, len(verts) - 1):
-                    # Create triangle indices (0, i, i+1)
-                    tri_indices = [0, i, i + 1]
-
-                    # Create triangle vertices with offset
-                    tri_verts = [
-                        vert_coords[idx] + vert_normals[idx] * offset
-                        for idx in tri_indices
+                    # First triangle
+                    tri1_verts = [
+                        vert_coords[i] + vert_normals[i] * offset for i in tri1_indices
                     ]
-                    self.ngons_data.extend(tri_verts)
-                    self.ngons_normals.extend([matrix_world.to_3x3() @ face.normal] * 3)
+                    self.quads_data.extend(tri1_verts)
+                    self.quads_normals.extend([matrix_world.to_3x3() @ face.normal] * 3)
+
+                    # Second triangle
+                    tri2_verts = [
+                        vert_coords[i] + vert_normals[i] * offset for i in tri2_indices
+                    ]
+                    self.quads_data.extend(tri2_verts)
+                    self.quads_normals.extend([matrix_world.to_3x3() @ face.normal] * 3)
+                elif (len(face.verts) > 4) and props.show_ngons:  # N-gon
+                    # ngons_to_process.append(face)
+                    verts = face.verts[:]
+                    vert_normals = [matrix_world.to_3x3() @ v.normal for v in verts]
+                    vert_coords = [matrix_world @ v.co for v in verts]
+
+                    # Create triangles by fanning from the first vertex
+                    for i in range(1, len(verts) - 1):
+                        # Create triangle indices (0, i, i+1)
+                        tri_indices = [0, i, i + 1]
+
+                        # Create triangle vertices with offset
+                        tri_verts = [
+                            vert_coords[idx] + vert_normals[idx] * offset
+                            for idx in tri_indices
+                        ]
+                        self.ngons_data.extend(tri_verts)
+                        self.ngons_normals.extend(
+                            [matrix_world.to_3x3() @ face.normal] * 3
+                        )
 
         # Free BMesh
         bm.free()
