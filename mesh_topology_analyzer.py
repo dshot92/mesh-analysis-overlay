10ee554fa13ea092d740217243dcb6e64f3aada5
dshot92 <dshot92@gmail.com>
2024-10-25 19:46:20 +0200

    feat: order mesh topology check, verts, edges and then faces.

--- mesh_topology_analyzer.py
+++ mesh_topology_analyzer.py
@@ -34,40 +34,11 @@ class MeshTopologyAnalyzer:
         bm.verts.ensure_lookup_table()
         bm.edges.ensure_lookup_table()
 
+        # recalculate bm normals
+        bmesh.ops.recalc_face_normals(bm, faces=bm.faces)
+
         # Clear previous data
         self.clear_data()
-
-        # Store ngons for later processing
-        ngons_to_process = []
-
-        # Analyze faces
-        for face in bm.faces:
-            verts = [matrix_world @ v.co for v in face.verts]
-            normal = matrix_world.to_3x3() @ face.normal
-
-            if len(face.verts) == 3:  # Triangle
-                self.tris_data.extend(verts)
-                self.tris_normals.extend([normal] * 3)
-            elif len(face.verts) == 4:  # Quad
-                tri1 = [verts[0], verts[1], verts[2]]
-                tri2 = [verts[2], verts[3], verts[0]]
-                self.quads_data.extend(tri1 + tri2)
-                self.quads_normals.extend([normal] * 6)
-            elif len(face.verts) > 4:  # N-gon
-                ngons_to_process.append((face, verts, normal))
-
-        # Process ngons : triangulate
-        for face, verts, normal in ngons_to_process:
-            temp_bm = bmesh.new()
-            temp_bm.faces.new([temp_bm.verts.new(v.co) for v in face.verts])
-            result = bmesh.ops.triangulate(
-                temp_bm, faces=temp_bm.faces, ngon_method="EAR_CLIP"
-            )
-            for new_face in result["faces"]:
-                new_verts = [matrix_world @ v.co for v in new_face.verts]
-                self.ngons_data.extend(new_verts)
-                self.ngons_normals.extend([normal] * 3)
-            temp_bm.free()
 
         # Analyze vertices for poles and singles
         for v in bm.verts:
@@ -88,6 +59,48 @@ class MeshTopologyAnalyzer:
                 v1 = matrix_world @ edge.verts[0].co
                 v2 = matrix_world @ edge.verts[1].co
                 self.non_manifold_edges_data.extend([v1, v2])
+
+        # Analyze faces
+        tris_to_process = []
+        quads_to_process = []
+        ngons_to_process = []
+
+        for face in bm.faces:
+            if len(face.verts) == 3:  # Triangle
+                tris_to_process.append(face)
+            elif len(face.verts) == 4:  # Quad
+                quads_to_process.append(face)
+            elif len(face.verts) > 4:  # N-gon
+                ngons_to_process.append(face)
+
+        # Process triangles
+        for face in tris_to_process:
+            verts = [matrix_world @ v.co for v in face.verts]
+            normal = matrix_world.to_3x3() @ face.normal
+            self.tris_data.extend(verts)
+            self.tris_normals.extend([normal] * 3)
+
+        # Process quads: triangulate
+        for face in quads_to_process:
+            face_copy = face.copy()
+            result = bmesh.ops.triangulate(bm, faces=[face_copy], quad_method="BEAUTY")
+            for new_face in result["faces"]:
+                normal = matrix_world.to_3x3() @ face.normal
+                new_verts = [matrix_world @ v.co for v in new_face.verts]
+                self.quads_data.extend(new_verts)
+                self.quads_normals.extend([normal] * 3)
+
+        # Process ngons: triangulate
+        for face in ngons_to_process:
+            face_copy = face.copy()
+            result = bmesh.ops.triangulate(
+                bm, faces=[face_copy], ngon_method="EAR_CLIP"
+            )
+            for new_face in result["faces"]:
+                normal = matrix_world.to_3x3() @ face.normal
+                new_verts = [matrix_world @ v.co for v in new_face.verts]
+                self.ngons_data.extend(new_verts)
+                self.ngons_normals.extend([normal] * 3)
 
         # Free BMesh
         bm.free()
