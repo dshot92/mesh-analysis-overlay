377608a8e5887b2dfa42137f377de8628bc5d32e
dshot92 <dshot92@gmail.com>
2024-10-25 14:33:21 +0200

    feat: correct ngon triangulation in overlay

--- mesh_topology_analyzer.py
+++ mesh_topology_analyzer.py
@@ -1,5 +1,6 @@
 import bpy
 from mathutils import Vector
+import bmesh
 
 
 class MeshTopologyAnalyzer:
@@ -35,15 +36,26 @@ class MeshTopologyAnalyzer:
 
     def analyze_ngons(self, obj, offset_value):
         self.ngons = []
-        for face in obj.data.polygons:
-            if len(face.vertices) > 4:
-                offset = face.normal * offset_value
-                face_verts = [
-                    (obj.matrix_world @ Vector(obj.data.vertices[v].co)) + offset
-                    for v in face.vertices
-                ]
-                for i in range(1, len(face_verts) - 1):
-                    self.ngons.extend([face_verts[0], face_verts[i], face_verts[i + 1]])
+        bm = bmesh.new()
+        bm.from_mesh(obj.data)
+        bmesh.ops.recalc_face_normals(bm, faces=bm.faces)
+
+        # Get only ngon faces and their normals
+        ngon_faces = [f for f in bm.faces if len(f.verts) > 4]
+
+        # Triangulate only ngons
+        result = bmesh.ops.triangulate(
+            bm, faces=ngon_faces, quad_method="SHORT_EDGE", ngon_method="BEAUTY"
+        )
+
+        # Get the new triangulated faces from the result
+        for new_face in result["faces"]:
+            offset = new_face.normal * offset_value
+            for v in new_face.verts:
+                vert_pos = (obj.matrix_world @ Vector(v.co)) + offset
+                self.ngons.append(vert_pos)
+
+        bm.free()
 
     def analyze_poles(self, obj):
         self.poles = []
