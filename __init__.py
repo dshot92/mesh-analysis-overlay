13822fd57f301c9e640ef86e12d662ca3166ab9d
dshot92 <dshot92@gmail.com>
2026-01-21 20:14:25 +0100

    feat: extension reload

--- __init__.py
+++ __init__.py
@@ -3,17 +3,34 @@
 from . import operators, panels, properties, handlers
 from .overlay_controller import overlay_controller
 
-modules = (properties, operators, panels, handlers)
+modules = [properties, operators, panels, handlers]
 
 
 def register():
+    hot_reload()
     for module in modules:
-        module.register()
+        if hasattr(module, "register"):
+            module.register()
 
 
 def unregister():
-    for module in reversed(modules):
-        module.unregister()
-
+    for module in modules:
+        if hasattr(module, "unregister"):
+            module.unregister()
+    
     if overlay_controller.is_running:
         overlay_controller.stop()
+
+
+def hot_reload():
+    # Refresh submodules during development
+    import importlib
+    import sys
+    
+    # Reload modules
+    for module in modules:
+        importlib.reload(module)
+    
+    # Reload the overlay_controller module, not the instance
+    if __package__ in sys.modules:
+        importlib.reload(sys.modules[__package__])
